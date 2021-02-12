e56cda914b422b52dd108751b8c995ddcc3d63b6
Ernesto <erg@datalabmx.com>
2021-02-12 14:53:07 -0600

    Iniciando Proyecto

---
+++ src/main/java/com/upax/eralrago/upax_test/UpaxTestApplication.java
@@ -0,0 +1,13 @@
+package com.upax.eralrago.upax_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpaxTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpaxTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upax/eralrago/upax_test/UpaxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upax.eralrago.upax_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpaxTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
